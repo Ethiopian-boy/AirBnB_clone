1fde00a3f57645f97b87637a97d8c1e3d6408a7a
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-08-06 01:52:59 -0700

    Unit Tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,3 +6,48 @@ import unittest
 import os
 import pep8
 from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.base1 = BaseModel()
+        cls.base1.name = "Mulubrhan"
+        cls.base1.my_number = 20
+
+    @classmethod
+    def tearDown(cls):
+        del cls.base1
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+
+    def test_checking_for_functions(self):
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+
+    def test_init(self):
+        self.assertTrue(isinstance(self.base1, BaseModel))
+
+    def test_save(self):
+        self.base1.save()
+        self.assertNotEqual(self.base1.created_at, self.base1.updated_at)
+
+    def test_to_dict(self):
+        base1_dict = self.base1.to_dict()
+        self.assertEqual(self.base1.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(bas1_dict['created_at], ,str)
+        self.assertIsInstance(bas1_dict['updated_at], ,str)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+import unittest
+import os
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
