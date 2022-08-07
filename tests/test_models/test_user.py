366d982712f64758fbb875e124f5e97852e556fd
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-07 09:48:19 +0100

    test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,56 +1,16 @@
-#!usr/bin/python3
-
+#!/usr/bin/python3
+""" testing User """
 import unittest
-import os
+import pep8
 from models.user import User
-from models.base_model import BaseModel
 
+class User_testing(unittest.TestCase):
+    """ check BaseModel """
 
-class TestUser(unittest.TestCase):
-
-    @classmethod
-    def setUp(cls):
-        cls.my_user = User()
-        cls.my_user.first_name = "Best"
-        cls.my_user.last_name = "School"
-        cls.my_user.email = "AirBnB@bestschool.com"
-        cl.my_user.password = "use"
-
-    @classmethod
-    def tearDown(cls):
-        del cls.my_user
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-    def test_is_subclass(self):
-        self.assertTrue(issubclass(self.my_user.__class__, BaseModel), True)
-
-    def  test_checking_for_functions(self):
-        self.assertIsNotNone(User.__doc__)
-
-    def test_has_attributes(self):
-        self.assertTrue('email' in self.my_user.__dict__)
-        self.assertTrue('id' in self.my_user.__dict__)
-        self.assertTrue('created_at' in self.my_user.__dict__)
-        self.assertTrue('updated_at' in self.my_user.__dict__)
-        self.assertTrue('password' in self.my_user.__dict__)
-        self.assertTrue('first_name' in self.my_user.__dict__)
-        self.assertTrue('last_name' in self.my_user.__dict__)
-
-    def test_attributes_are_strings(self):
-        self.assertEqual(type(self.my_user.email), str)
-        self.assertEqual(type(self.my_user.first_name), str)
-        self.assertEqual(type(self.my_user.last_name), str)
-        self.assertEqual(type(self.my_user.password), str)
-
-    def test_save(self):
-        self.my_user.save()
-        self.assertNotEqual(self.my_user.created_at, self.my_user.updated_at)
-
-    def test_to_dict(self):
-        self.assertEqual('to_dict' in dir(self.my_user), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/user.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
