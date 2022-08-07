b361ad52bb7ad891a6a61ba4b3a029f8641acab1
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-07 09:46:31 +0100

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,49 +1,16 @@
 #!/usr/bin/python3
-
+""" testing State """
 import unittest
-import os
+import pep8
 from models.state import State
-from models.base_model import BaseModel
 
+class State_testing(unittest.TestCase):
+    """ check BaseModel """
 
-
-class TestState(unittest.TestCase):
-
-    @classmethod
-    def setU(cls):
-        cls.state1 = State()
-        cls.state1.name = "Gujarat"
-
-    @classmethod
-    def tearDown(cls):
-        del cls.state1
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-
-    def test_is_subclass(self):
-        self.assertTrue(issubclass(self.state1.__class__, BaseModel), True)
-
-    def test_checking_for_functions(self):
-        self.assertIsNotNone(State.__doc__)
-
-    def test_has_attributes(self):
-        self.assertTrue('id' in self.state1.__dict__)
-        self.assertTrue('created_at' in self.state1.__dict__)
-        self.assertTrue('updated_at' in self.state1.__dict__)
-        self.assertTrue('name' in self.state1.__dict__)
-
-    def test_attributes_are_str(self):
-        self.assertEqual(type(self.state1.name), str)
-
-    def test_save(self):
-        self.state1.save()
-        self.assertNotEqual(self.state1.created_at, self.state1.updated_at)
-
-    def test_to_dict(self):
-        self.assertEqual('to_dict' in dir(self.state1), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/state.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
