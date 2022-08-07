1cd44170fcffa359170cf452d3407c830a08e46f
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-07 09:42:38 +0100

    test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,51 +1,16 @@
 #!/usr/bin/python3
-
+""" testing city """
 import unittest
-import os
+import pep8
 from models.city import City
-from models.base_model import BaseModel
 
+class City_testing(unittest.TestCase):
+    """ check BaseModel """
 
-class TestCity(unittest.TestCase):
-
-    @classmethod
-    def setUp(cls):
-        cls.city1 = City()
-        cls.city1.name = "Rajkot"
-        cls.city1.state_id = "IN"
-
-    @classmethod
-    def tearDown(cls):
-        del cls.city1
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-
-    def test_is_subclass(self):
-        self.assertTrue(issubclass(self.city1.__class__, BaseModel), True)
-
-    def test_checking_fnctions(self):
-        self.assertIsNotNone(City.__doc__)
-
-    def test_has_attributes(self):
-        self.assertTrue('id' in self.city1.__dict__)
-        self.assertTrue('name' in self.city1.__dict__)
-        self.assertTrue('state_id' in self.city1.__dict__)
-        self.assertTrue('created_at' in self.city1.__dict__)
-        self.assertTrue('updated_at' in self.city1.__dict__)
-
-    def test_attribute_are_str(self):
-        self.assertEqual(type(self.city1.name), str)
-        self.assertEqual(type(self.city1.state_id), str)
-
-    def test_save(self):
-        self.city1.save()
-        self.assertNotEqual(self.city1.created_at, self.city1.updated_at)
-    
-    def test_to_dict(self):
-        self.assertEqual('to_dict' in dir(self.city1), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/city.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
